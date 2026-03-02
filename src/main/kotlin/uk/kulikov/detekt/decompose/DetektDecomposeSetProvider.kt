5d815781b72657488f97e6e3c32a9b25724c824c
Nikita Kulikov <n@kulikov.uk>
2026-03-02 17:27:51 +0000

    Update to Detekt 2.0

--- src/main/kotlin/uk/kulikov/detekt/decompose/DetektDecomposeSetProvider.kt
+++ src/main/kotlin/uk/kulikov/detekt/decompose/DetektDecomposeSetProvider.kt
@@ -1,19 +1,14 @@
 package uk.kulikov.detekt.decompose
 
-import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.RuleSet
-import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+import dev.detekt.api.RuleSet
+import dev.detekt.api.RuleSetId
+import dev.detekt.api.RuleSetProvider
 import uk.kulikov.detekt.decompose.rules.PushForbiddenRule
 
 class DetektDecomposeSetProvider : RuleSetProvider {
-    override val ruleSetId: String = "DecomposeRule"
+    override val ruleSetId = RuleSetId("DecomposeRule")
 
-    override fun instance(config: Config): RuleSet {
-        return RuleSet(
-            ruleSetId,
-            listOf(
-                PushForbiddenRule(config),
-            ),
-        )
+    override fun instance(): RuleSet {
+        return RuleSet(ruleSetId, listOf(::PushForbiddenRule))
     }
 }
--- src/main/kotlin/uk/kulikov/detekt/decompose/rules/PushForbiddenRule.kt
+++ src/main/kotlin/uk/kulikov/detekt/decompose/rules/PushForbiddenRule.kt
@@ -1,22 +1,22 @@
 package uk.kulikov.detekt.decompose.rules
 
-import io.gitlab.arturbosch.detekt.api.*
-import io.gitlab.arturbosch.detekt.rules.isCalling
+import dev.detekt.api.Config
+import dev.detekt.api.Entity
+import dev.detekt.api.Finding
+import dev.detekt.api.RequiresAnalysisApi
+import dev.detekt.api.Rule
+import dev.detekt.psi.isCalling
+import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.resolve.ImportPath
 
 class PushForbiddenRule(config: Config) : Rule(
-    config
-) {
-    private val description = getDescription(config)
-    override val issue = Issue(
-        "PushForbiddenRule",
-        Severity.CodeSmell,
-        description,
-        Debt.FIVE_MINS,
-    )
+    config,
+    getDescription(config),
+), RequiresAnalysisApi {
     private val checkImport = config.valueOrDefault("checkImport", true)
 
     override fun visitImportDirective(importDirective: KtImportDirective) {
@@ -27,33 +27,36 @@ class PushForbiddenRule(config: Config) : Rule(
         }
 
         if (importDirective.importPath == importPath) {
-            report(CodeSmell(issue, Entity.from(importDirective), issue.description))
+            report(Finding(Entity.from(importDirective), description))
         }
     }
 
     override fun visitCallExpression(expression: KtCallExpression) {
         super.visitCallExpression(expression)
 
-        if (expression.isCalling(requireFunctionFqName, bindingContext)) {
-            report(CodeSmell(issue, Entity.from(expression), issue.description))
-        }
-    }
-
-    private fun getDescription(config: Config): String {
-        var replaceTo = config.valueOrNull<String>("replaceTo")
-        if (replaceTo == null) {
-            replaceTo = config.subConfig("PushForbiddenRule")
-                .valueOrNull("replaceTo")
-        }
-        return if (replaceTo == null) {
-            "The push() method can cause crashes in runtime. Use safer ways to add a screen to the stack. More information: https://arkivanov.github.io/Decompose/navigation/stack/navigation/#stacknavigator-extension-functions"
-        } else {
-            "Use $replaceTo instead of push() to avoid runtime crashes"
+        if (expression.isCalling(requireFunctionCallableId)) {
+            report(Finding(Entity.from(expression), description))
         }
     }
 
     companion object {
-        private val requireFunctionFqName = FqName("com.arkivanov.decompose.router.stack.push")
+        private val requireFunctionCallableId = CallableId(
+            FqName("com.arkivanov.decompose.router.stack"),
+            Name.identifier("push"),
+        )
         private val importPath = ImportPath.fromString("com.arkivanov.decompose.router.stack.push")
+
+        private fun getDescription(config: Config): String {
+            var replaceTo = config.valueOrNull<String>("replaceTo")
+            if (replaceTo == null) {
+                replaceTo = config.subConfig("PushForbiddenRule")
+                    .valueOrNull("replaceTo")
+            }
+            return if (replaceTo == null) {
+                "The push() method can cause crashes in runtime. Use safer ways to add a screen to the stack. More information: https://arkivanov.github.io/Decompose/navigation/stack/navigation/#stacknavigator-extension-functions"
+            } else {
+                "Use $replaceTo instead of push() to avoid runtime crashes"
+            }
+        }
     }
 }
